9e5b19adf9c43238aa8b936d2d0fa0f8a729efd9
Richard Chou <konami99@hotmail.com>
2024-01-01 12:59:44 +1100

    updated styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,10 +10,16 @@ export default async function Home() {
 
   if (events.length > 0) {
     return (
-      <section id="todays-events"style={{background: 'linear-gradient(rgb(44, 157, 206) 0%, rgb(80, 159, 185) 30%, rgb(174, 235, 255) 100%);'}}>
+      <section className="todays-events pt-[6rem] pb-[3rem]" style={{background: 'linear-gradient(rgb(44, 157, 206) 0%, rgb(80, 159, 185) 30%, rgb(174, 235, 255) 100%);'}}>
+        <header className="relative flex flex-col items-center py-4">
+          <div className='subtitle ml-4 w-full'>Up next</div>
+          <div className='title ml-4 w-full'>
+            <h2><span>What's on</span> this afternoon</h2>
+          </div>
+        </header>
         <div className="relative flex items-center p-4">
           <ChevronLeft />
-          <div id="slider" className='my-32 w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+          <div id="slider" className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
             {
               events.map((event, index) => (
                 <EventCard key={index} event={event} />
