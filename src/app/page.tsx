7e64e00c7ebd79cb0c670e950b2946fb1fe69cd2
Richard Chou <konami99@hotmail.com>
2024-01-07 10:55:24 +1100

    updated layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { useEffect } from 'react';
 
 export default async function Home() {
 
-  const events = await getEvents({
+  const today_events = await getEvents({
     where: {
         startDate: {
             lte: new Date(new Date().setDate(new Date().getDate() - 1)),
@@ -20,28 +20,47 @@ export default async function Home() {
     }
   });
 
-  if (events.length > 0) {
-    return (
-      <>
+  const selected_events = await getEvents({
+    where: {
+        startDate: {
+            lte: new Date(new Date().setDate(new Date().getDate() - 1)),
+        },
+        endDate: {
+            gte: new Date(new Date().setDate(new Date().getDate() + 1)),
+        },
+        status: {
+            eq: 'approved'
+        },
+        pick: {
+          eq: true,
+        },
+    }
+  });
+
+  return (
+    <>
+      {today_events.length > 0 &&
         <section className="todays-events pt-[6rem] pb-[3rem]" style={{background: 'linear-gradient(rgb(44, 157, 206) 0%, rgb(80, 159, 185) 30%, rgb(174, 235, 255) 100%)'}}>
-          <header className="relative flex flex-col items-center py-4 my-0 mx-[calc(2rem+28.21px)]">
-            <div className='subtitle ml-4 w-full text-transform: uppercase text-[0.75rem]'>Up next</div>
-            <div className='title ml-4 w-full text-[1.75rem] text-white'>
-              <h2><span className='text-black'>What's on</span> this afternoon</h2>
-            </div>
-          </header>
-          <div className="relative flex items-center p-4">
-            <ChevronLeft targetId={ 'slider-todays-events' } />
-            <div id="slider-todays-events" className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
-              {
-                events.map((event, index) => (
-                  <EventCard key={index} event={event} />
-                ))
-              }
-            </div>
-            <ChevronRight targetId={ 'slider-todays-events' } />
+        <header className="relative flex flex-col items-center py-4 my-0 mx-[calc(2rem+28.21px)]">
+          <div className='subtitle ml-4 w-full text-transform: uppercase text-[0.75rem]'>Up next</div>
+          <div className='title ml-4 w-full text-[1.75rem] text-white'>
+            <h2><span className='text-black'>What's on</span> this afternoon</h2>
           </div>
+        </header>
+        <div className="relative flex items-center p-4">
+          <ChevronLeft targetId={ 'slider-todays-events' } />
+          <div id="slider-todays-events" className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
+            {
+              today_events.map((event, index) => (
+                <EventCard key={index} event={event} />
+              ))
+            }
+          </div>
+          <ChevronRight targetId={ 'slider-todays-events' } />
+        </div>
         </section>
+      }
+      {selected_events.length > 0 &&
         <section className="our-picks pt-[6rem] pb-[3rem] bg-white">
           <header className="relative flex flex-col items-center py-4 my-0 mx-[calc(2rem+28.21px)]">
             <div className='subtitle ml-4 w-full text-transform: uppercase text-[0.75rem]'>Upcoming events</div>
@@ -53,7 +72,7 @@ export default async function Home() {
             <ChevronLeft targetId={ 'slider-our-picks' } />
             <div id="slider-our-picks" className='w-full h-full overflow-x-scroll scroll whitespace-nowrap scroll-smooth scrollbar-hide'>
               {
-                events.map((event, index) => (
+                selected_events.map((event, index) => (
                   <EventCard key={index} event={event} />
                 ))
               }
@@ -61,9 +80,7 @@ export default async function Home() {
             <ChevronRight targetId={ 'slider-our-picks' } />
           </div>
         </section>
-      </>
-    )
-  } else {
-    return <div className="relative flex items-center">No Events Found</div>
-  }
+      }
+    </>
+  )
 }
