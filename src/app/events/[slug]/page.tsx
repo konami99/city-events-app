7f22d2705d29da2426ec889899831831d9e7c4ef
Richard Chou <konami99@hotmail.com>
2024-01-06 15:41:52 +1100

    more styling on page

--- src/app/events/[slug]/page.tsx
+++ src/app/events/[slug]/page.tsx
@@ -23,7 +23,7 @@ export default async function EventPage({
     const event = await getEvent(slug);
 
     return (
-        <div>
+        <div className="bg-white">
             <div className="header h-[210px] flex justify-center items-center">
                 <div className="header-container">
                     <div className="m-[1rem]">
@@ -39,14 +39,17 @@ export default async function EventPage({
             <div className="py-0 lg:px-[8rem] md:px-[1rem] h-[350px]" style={{background: 'linear-gradient(to top, rgb(4, 28, 44) 0%, rgb(4, 28, 44) 5rem, rgba(4, 28, 44, 0) 5.0625rem, rgba(4, 28, 44, 0) 100%)'}}>
                 <div className="hero h-[350px]" style={{backgroundImage: `url(${urlFor(event.mainImage.asset._id).width(360).height(350).url()})`}}></div>
             </div>
-            <div className="event-content py-[5rem] lg:px-[8rem] px-[1rem]">
+            <div className="event-content py-[5rem] lg:px-[8rem] md:px-[1rem]">
                 <div className="event-content-container grid grid-cols-1 md:grid-cols-3 m-auto max-w-[65rem]">
-                    <div className="event-description col-span-1 md:col-span-2">
-                    <PortableText value={ event.descriptionRaw } />
+                    <div className="event-description col-span-1 md:col-span-2 md:pr-[7.25rem] max-md:px-[1rem] max-md:mb-[4rem]">
+                        <PortableText value={ event.descriptionRaw } />
                     </div>
                     <div className="event-sidebar-container">
-                        <div className="event-sidebar">
-                            sidebar
+                        <div className="event-sidebar bg-neutral-100 pt-[1.5rem] px-[2rem] pb-[2.5rem]">
+                            <div className="event-sidebar-section">
+                                <p className="event-heading text-xl mb-[1rem] font-medium">Contact event organiser</p>
+                                <p>{ event.eventOrganiser }</p>
+                            </div>
                         </div>
                     </div>
                 </div>
--- src/lib/db/event.ts
+++ src/lib/db/event.ts
@@ -75,6 +75,7 @@ export async function getEvent(slug: String) {
                             }
                         }
                         descriptionRaw
+                        eventOrganiser
                     }
                 }`,
         variables: {
