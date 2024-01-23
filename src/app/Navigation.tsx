003b44b698c2a97a3679d7b84fb755940c5a5929
Richard Chou <konami99@hotmail.com>
2024-01-23 21:51:31 +1100

    added navigation

---
+++ src/app/Navigation.tsx
@@ -0,0 +1,21 @@
+export default function Navigation() {
+    return (
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+                </div>
+                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                    <li><a>Log in</a></li>
+                    <li><a>List your event for free</a></li>
+                </ul>
+            </div>
+          </div>
+          <div className="navbar-center">
+            <a className="btn btn-ghost text-xl" href="/">What's On</a>
+          </div>
+          <div className="navbar-end"></div>
+        </div>
+    )
+}
