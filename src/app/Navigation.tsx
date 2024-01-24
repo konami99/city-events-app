1499d78c5a4ea7edd9e13941f668c8c6428f0243
Richard Chou <konami99@hotmail.com>
2024-01-24 17:38:54 +1100

    added auth

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,21 +1,36 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Navigation() {
-    return (
-        <div className="navbar bg-base-100">
-          <div className="navbar-start">
-            <div className="dropdown">
-                <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                </div>
-                <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><a>Log in</a></li>
-                    <li><a>List your event for free</a></li>
-                </ul>
-            </div>
-          </div>
-          <div className="navbar-center">
-            <a className="btn btn-ghost text-xl" href="/">What&apos;s On</a>
-          </div>
-          <div className="navbar-end"></div>
-        </div>
-    )
+  const { data: session } = useSession();
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start"></div>
+      <div className="navbar-center">
+        <a className="btn btn-ghost text-xl" href="/">What&apos;s On</a>
+      </div>
+      <div className="navbar-end">
+        {
+          session && session.user ?
+            <ul className="menu menu-horizontal px-1">
+              <li>
+                <details>
+                  <summary>Hi { session.user.email }</summary>
+                  <ul className="p-2">
+                    <li><a onClick={() => signOut()}>Log out</a></li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          :
+            <ul className="menu menu-horizontal px-1">
+              <li><a onClick={() => signIn()}>Log in</a></li>
+              <li><a onClick={() => signIn()}>List your event for free</a></li>
+            </ul>
+        }
+      </div>
+    </div>
+  )
 }
